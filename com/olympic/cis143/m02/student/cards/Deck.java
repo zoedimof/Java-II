ada39cb37e0a938b6e248255b7814704613eb1fd
zoedimof <63650422+zoedimof@users.noreply.github.com>
2022-05-04 15:44:41 -0700

    Add files via upload

---
+++ com/olympic/cis143/m02/student/cards/Deck.java
@@ -0,0 +1,11 @@
+package com.olympic.cis143.m02.student.cards;
+
+import java.util.Stack;
+
+public interface Deck {
+
+	public Stack<Card> getDeck();
+	public void shuffle();
+	public boolean hasNext();
+	public Card dealCard();
+}
